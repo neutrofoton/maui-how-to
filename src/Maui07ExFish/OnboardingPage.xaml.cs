d37c93206da6a0ac5f224f16951e32b94e0c3b29
neutrofoton <neutrofoton@gmail.com>
2022-07-26 23:38:54 +0700

    add animated carusel

--- src/Maui07ExFish/OnboardingPage.xaml.cs
+++ src/Maui07ExFish/OnboardingPage.xaml.cs
@@ -1,13 +1,18 @@
 ﻿namespace Maui07ExFish;
 
+using System.Timers;
+using Maui07ExFish.ViewModels;
+
 public partial class OnboardingPage : ContentPage
 {
-	int count = 0;
+	private Timer timer;
 
 	public OnboardingPage()
 	{
 		InitializeComponent();
-	}
+        AnimateCarusel();
+
+    }
 
 	private async void Button_Clicked(object sender, EventArgs e)
 	{
@@ -16,5 +21,31 @@ public partial class OnboardingPage : ContentPage
 
 		
 	}
+
+    private void AnimateCarusel()
+    {
+        timer = new Timer(5000)
+        {
+            AutoReset = true,
+            Enabled = true
+        };
+
+        OnboardingViewModel onboardingViewModel = this.BindingContext as OnboardingViewModel;
+        int caruselItems = onboardingViewModel.Items.Count;
+
+        timer.Elapsed += (sender, e) =>
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+                if (fishingCaruselView.Position == caruselItems - 1)
+                {
+                    fishingCaruselView.Position = 0;
+                    return;
+                }
+
+                fishingCaruselView.Position += 1;
+            });
+        };
+    }
 }
 
